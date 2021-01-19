3b46e8cb7486bd3f0e53bbfad42840f715435736
Anuradha Aggarwal <anuradha.mcs18.du@gmail.com>
2021-01-20 03:37:14 +0530

    Infinite scroller added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import apiService from './services/index'; 
-import React,  {useEffect} from 'react';
+import apiService from './services/index';
+import React, { useEffect, useState } from 'react';
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 function App() {
 
-  useEffect(()=>{
-    getMovieList(1);
-  },[])
+  const [movieList, setMovieList] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
 
-  const getMovieList = async (pageNo) => {
+  useEffect(() => {
+    getMovieList();
+  }, [])
+
+  const getMovieList = async () => {
     //event.preventDefault()
     try {
-      const movieList = await apiService.getMovieList(pageNo)
-      console.log("movieList--> ", movieList);
-    } 
+      const apiResponse = await apiService.getMovieList(pageNumber + 1);
+      setPageNumber(pageNumber + 1);
+      console.log("movieList--> ", apiResponse);
+      const newList = [...movieList, ...apiResponse.results ]
+      setMovieList(newList);
+    }
     catch (exception) {
       console.log("error")
     }
@@ -24,17 +32,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <InfiniteScroll
+          dataLength={movieList.length}
+          next={getMovieList}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
         >
-          Learn React
-        </a>
+          {movieList && movieList.length > 0 ?
+            movieList.map((item, key) => (
+              <div key={key}>
+                {item.overview} <hr></hr>
+              </div>
+            ))
+            : ''
+          }
+        </InfiniteScroll>
 
       </header>
     </div>
