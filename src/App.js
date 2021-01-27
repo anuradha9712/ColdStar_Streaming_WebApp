41b1e7b8a2b9c17199ee8c79f7ebc048ac929098
Anuradha Aggarwal <anuradha.mcs18.du@gmail.com>
2021-01-28 02:47:17 +0530

    search bar functionality

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import apiService from './services/index';
 import React, { useEffect, useState } from 'react';
 import InfiniteScroll from "react-infinite-scroll-component";
 import MovieCard from './components/MovieCard/index';
-import { Grid, Typography, TextField } from '@material-ui/core';
+import { Grid, Typography, TextField, Button } from '@material-ui/core';
 
 function App() {
 
   const [movieList, setMovieList] = useState([]);
+  const [originalMovieList, setOriginalMovieList] = useState([]);
   const [pageNumber, setPageNumber] = useState(1);
+  const [searchName, setSearchName] = useState('');
+  const [showSearchList, setShowSearchList] = useState(false);
   const index = Math.floor(Math.random() * 10);
 
   useEffect(() => {
@@ -23,15 +26,35 @@ function App() {
       setPageNumber(pageNumber + 1);
       console.log("movieList--> ", apiResponse);
       const newList = [...movieList, ...apiResponse.results]
-      setMovieList(newList);
+      setMovieList(newList.filter((item)=> item.backdrop_path !== null && item.poster_path !== null));
+      setOriginalMovieList(newList.filter((item)=> item.backdrop_path !== null && item.poster_path !== null));
+
     }
     catch (exception) {
       console.log("error")
     }
   }
 
-  const handleFilterNameChange = () => {
-    console.log("inside filter")
+  const handleFilterNameChange = (event) => {
+    console.log("inside filter--> ", event.target.value);
+    setSearchName(event.target.value);
+    if(event.target.value === ''){
+      setShowSearchList(false);
+      setMovieList(originalMovieList);
+    }
+  }
+
+  const handleSearchChange = async () => {
+    try {
+      const apiResponse = await apiService.searchMovie(searchName);
+      console.log("search movieList--> ", apiResponse);
+      const newList = [...apiResponse.results];
+      setMovieList(newList.filter((item)=> item.backdrop_path !== null && item.poster_path !== null));
+      setShowSearchList(true);
+    }
+    catch (exception) {
+      console.log("error")
+    }
   }
 
   return (
@@ -42,14 +65,19 @@ function App() {
           ColdStar
         </Grid>
         <Grid item xs={6} >
-          <Grid item className="searchbox-container">
-            <TextField id="outlined-basic3" label="Search" onChange={handleFilterNameChange} variant="outlined" fullWidth />
+          <Grid item container xs={12} spacing={2} className="searchbox-container">
+            <Grid item xs={8}>
+              <TextField id="outlined-basic3" label="Search" onChange={handleFilterNameChange} variant="outlined" fullWidth />
+            </Grid>
+            <Grid item xs={4}>
+              <Button variant="contained" onClick={handleSearchChange} color="secondary" size="large" className="searchBtn">Search</Button>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
 
       {/* ==========================Banner====================== */}
-      {movieList && movieList[0] ?
+      {movieList && movieList[index] ?
         <Grid container xs={12} className="main-banner" style={{ background: `linear-gradient(rgba(32, 32, 32, 0) 0%, #202020 100%), url(http://image.tmdb.org/t/p/w1280${movieList[index].backdrop_path})` }}>
 
           <Grid item xs={12} className="banner-text">
@@ -63,7 +91,7 @@ function App() {
       {/* ==========================Movie List====================== */}
       <InfiniteScroll
         dataLength={movieList.length}
-        next={getMovieList}
+        next={showSearchList ? '' : getMovieList}
         hasMore={true}
         loader={<h4>Loading...</h4>}
       >
--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,13 @@
 import axios from 'axios'
-const baseUrl = 'https://api.themoviedb.org/3/movie/popular'
+const baseUrl = 'https://api.themoviedb.org/3'
 
 const getMovieList = async pageNo => {
-  const response = await axios.get(`${baseUrl}?api_key=${process.env.REACT_APP_API_KEY}&page=${pageNo}`)
+  const response = await axios.get(`${baseUrl}/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&page=${pageNo}`)
   return response.data
 }
 
-export default { getMovieList }
+const searchMovie = async movieName =>{
+  const response = await axios.get(`${baseUrl}/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${movieName}`)
+  return response.data
+}
+export default { getMovieList, searchMovie }
